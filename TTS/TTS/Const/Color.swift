20ef5567ba1d843ad163184b30a34f3d470b949c
JunYoung Lee <37971925+rubinstory@users.noreply.github.com>
2022-09-04 15:38:27 +0900

    feat: Add Color (#10)
    공통 색상 사용을 위한 파일을 추가했습니다.

---
+++ TTS/TTS/Const/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  TTS
+//
+//  Created by Lee Jun Young on 2022/09/04.
+//
+
+import UIKit
+
+extension Const {
+    struct Color {
+        static let primary: UIColor = .blue.withAlphaComponent(0.5)
+    }
+}
