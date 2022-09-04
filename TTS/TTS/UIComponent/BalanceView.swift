3179b0cf2a72f12c94011ca74306ea49ce1d5f99
JunYoung Lee <37971925+rubinstory@users.noreply.github.com>
2022-09-04 15:40:36 +0900

    feat: Add VC and view for supplier view (#11)
    판매자 화면을 위한 VC와 View를 추가

---
+++ TTS/TTS/UIComponent/BalanceView.swift
@@ -0,0 +1,13 @@
+//
+//  BalanceView.swift
+//  TTS
+//
+//  Created by Lee Jun Young on 2022/09/04.
+//
+
+import UIKit
+
+class BalanceView: UIView {
+    private var iconView = UIImageView()
+    
+}
---
+++ TTS/TTS/VC/SupplierInfoVC.swift
@@ -0,0 +1,22 @@
+//
+//  BalanceVCX.swift
+//  TTS
+//
+//  Created by Lee Jun Young on 2022/09/04.
+//
+
+import UIKit
+
+class SupplierInfoVC: UIViewController {
+    
+    private var balanceView = UIView()
+    private var profitView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setBalanceView() {
+        
+    }
+}
