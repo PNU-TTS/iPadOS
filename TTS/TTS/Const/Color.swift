3697634d380a0f249c0e7e81b8cb46daf86cb1fd
Junyoung Lee <rubinstory@naver.com>
2022-09-08 01:20:32 +0900

    feat: Add Color

--- TTS/TTS/Const/Color.swift
+++ TTS/TTS/Const/Color.swift
@@ -10,5 +10,15 @@ import UIKit
 extension Const {
     struct Color {
         static let primary: UIColor = .blue.withAlphaComponent(0.5)
+        
+        static let semanticRed1 = UIColor(named: "SemanticRed1")!
+        static let semanticGreen1 = UIColor(named: "SemanticGreen1")!
+        static let semanticYellow1 = UIColor(named: "SemanticYellow1")!
+        static let semanticBlue1 = UIColor(named: "SemanticBlue1")!
+        
+        static let semanticRed2 = UIColor(named: "SemanticRed2")!
+        static let semanticGreen2 = UIColor(named: "SemanticGreen2")!
+        static let semanticYellow2 = UIColor(named: "SemanticYellow2")!
+        static let semanticBlue2 = UIColor(named: "SemanticBlue2")!
     }
 }
